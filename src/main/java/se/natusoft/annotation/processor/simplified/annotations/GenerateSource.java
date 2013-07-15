f4debb20e03e632e2159f2efa998dc4d46cd028a
tommy <tommy@natusoft.se>
2013-07-15 11:57:43 +0200

    Oops! Wrong package name for processors in META-INF/services/javax.annotation.processing.Processor.

--- src/main/java/se/natusoft/annotation/processor/simplified/annotations/GenerateSource.java
+++ src/main/java/se/natusoft/annotation/processor/simplified/annotations/GenerateSource.java
@@ -29,7 +29,12 @@
  *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *     See the License for the specific language governing permissions and
  *     limitations under the License.
- *
+ *     
+ * AUTHORS
+ *     tommy ()
+ *         Changes:
+ *         2013-07-15: Created!
+ *         
  */
 package se.natusoft.annotation.processor.simplified.annotations;
 
--- src/main/java/se/natusoft/annotation/resource/processor/ResourcePathProcessor.java
+++ src/main/java/se/natusoft/annotation/resource/processor/ResourcePathProcessor.java
@@ -30,6 +30,11 @@
  *     See the License for the specific language governing permissions and
  *     limitations under the License.
  *     
+ * AUTHORS
+ *     tommy ()
+ *         Changes:
+ *         2013-07-15: Created!
+ *         
  */
 package se.natusoft.annotation.resource.processor;
 
